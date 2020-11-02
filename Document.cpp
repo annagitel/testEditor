eddde52a64acbea0c2e0d652e0aaba3bc1213daf
annagitel <asandler@redhat.com>
2020-11-02 17:41:18 +0200

    90 grade

--- Document.cpp
+++ Document.cpp
@@ -5,92 +5,105 @@
 using namespace std;
 
 Document::Document(){
-	currentLine = 0;
+	this->currentLine = 0;
+	cout << "default:document is inited with " << this->doc.size() << endl;
 }
 
 Document::Document(string filename) {
-	currentLine = 0;
 	std::ifstream infile(filename);
 	std::string line;
 	while (std::getline(infile, line)){
 		doc.push_back(line);
 	}
+	this->currentLine = doc.size()-1;
+	cout << "with file: document is inited with " << this->doc.size() << endl;
 }
 
 Document::~Document(){}
 
 void Document::changeCurrentLine(int newLine){
 	this->currentLine = newLine-1 ;
-	cout << "now on line: " << this->currentLine << endl;
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 
 void Document::xlinesForward(int x){
 	this->currentLine = this->currentLine + x;
-	cout << "now on line: " << this->currentLine << endl;
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 
 void Document::xlinesBack(int x){
 	this->currentLine = this->currentLine - x;
-	cout << "now on line: " << this->currentLine << endl;
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 
 void Document::lastLine(){
-	this->currentLine = doc.size();
-	cout << "now on line: " << this->currentLine << endl;
+	this->currentLine = doc.size()-1;
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 
 void Document::addAfterCurrent(){
 	string in = "";
-		while (getline(cin, in)){
-			if(in.compare(".") == 0)
-				break;
-			if(this->doc.size() == 0)
-				this->doc.push_back(in);
-			else
-				this->doc.insert(this->doc.begin() + this->currentLine, in);
-			this->currentLine++;
-		}
+    while (getline(cin, in)){
+
+        if(in.compare(".") == 0)
+            break;
+
+        if(this->doc.size() == 0)
+            this->doc.push_back(in);
+
+        else // insert after
+            this->doc.insert(this->doc.begin() + this->currentLine, in);
+
+        this->currentLine++;
+    }
 }
+
 void Document::addBeforeCurrent(){
-	string in = "";
-		while (getline(cin, in)){
-			if(in.compare(".") == 0)
-				break;
-			if(this->doc.size() == 0)
-				this->doc.push_back(in);
-			else
-				this->doc.insert(this->doc.begin() + this->currentLine -1, in);
-			this->currentLine++;
-		}
+string in = "";
+    while (getline(cin, in)){
+
+        if(in.compare(".") == 0)
+			break;
+
+        if(this->doc.size() == 0)
+            this->doc.push_back(in);
+
+        else // insert before
+            this->doc.insert(this->doc.begin() + this->currentLine, in);
+        this->currentLine++;
+    }
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 
 void Document::changeCurrent(){
-	deleteCurrent();
-	addBeforeCurrent();
-
+	this->doc.erase(this->doc.begin() + this->currentLine-1);
+    addAfterCurrent();
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 void Document::deleteCurrent(){
 	this->doc.erase(this->doc.begin()-1 + this->currentLine--);
+	cout << "now on line: " << this->currentLine+1 << endl;
 }
 void Document::search(string s){
-	for(auto it = this->doc.begin() + this->currentLine -1; it != this->doc.end(); ++it){
+	for(auto it = this->doc.begin() + this->currentLine ; it != this->doc.end(); ++it){
 
         string line = *it;
         auto find = line.find(s);
 
          if(find != string::npos){
             cout << *it << endl;
-            this->currentLine = distance(this->doc.begin(), it) +1;
+            this->currentLine = distance(this->doc.begin(), it) ;
+            cout << "now on line: " << this->currentLine+1 << endl;
             return;
         }
     }
-    this->currentLine = this->doc.size();
-	for(auto it = this->doc.begin() + this->currentLine -1; it != this->doc.begin(); --it){
+	for(auto it = this->doc.begin() ; it != this->doc.begin() + this->currentLine; ++it){
         string word = *it;
         auto find = word.find(s);
         if(find != string::npos){
             cout << *it << endl;
-            this->currentLine = distance(this->doc.begin(), it) +1;
+            this->currentLine = distance(this->doc.begin(), it) ;
+			cout << "now on line: " << this->currentLine+1 << endl;
             return;
         }
     }
@@ -98,13 +111,15 @@ void Document::search(string s){
 }
 void Document::replaceText(string s){
 	string olds = s.substr(0,s.find("/"));
-	string news = s.substr(s.find("/")+1, s.size() - olds.size() -2);
-	this->doc.at(currentLine -1).replace(this->doc.at(currentLine -1).find(olds), olds.size(), news);
+	cout << "old word " << olds << endl;
+	string news = s.substr(s.find("/")+1, s.size() - 1);
+	cout << "new word " << news << endl;
+	this->doc.at(currentLine).replace(this->doc.at(currentLine).find(olds), olds.size(), news);
 }
 void Document::combine(){
 	string s = "";
 	doc[currentLine].append(doc[currentLine+1]);
-	this->doc.erase(this->doc.begin() + this->currentLine--);
+	this->doc.erase(this->doc.begin() + this->currentLine+1);
 }
 void Document::writeToFile(string filename){
 	std::ofstream outFile(filename);
--- Editor.cpp
+++ Editor.cpp
@@ -13,35 +13,62 @@ void Editor::loop(){
     string input="";
 
     while(getline(cin,input)){
-
-    	if (input.at(0)=='q')
-    		return;
-    	else if (input.at(0) < '0' && input.at(0) <= '9')
-    		this->ed.changeCurrentLine(stoi(input));
-    	else if (input.at(0) == '+')
-    		this->ed.xlinesForward(stoi(input.substr(1,input.size()-1)));
-    	else if (input.at(0) == '-')
-    		this->ed.xlinesBack(stoi(input.substr(1,input.size()-1)));
-    	else if (input.at(0)== '$')
-			this->ed.lastLine();
-    	else if (input.at(0) == 'a')
-    		this->ed.addAfterCurrent();
-    	else if (input.at(0) == 'i')
-    		this->ed.addBeforeCurrent();
-    	else if (input.at(0) == 'c')
-    		this->ed.changeCurrent();
-    	else if (input.at(0) == 'd')
-    		this->ed.deleteCurrent();
-    	else if (input.at(0) == '/')
-    		this->ed.search(input.substr(1,input.size()-1));
-    	else if (input.at(0) == 's')
-    		this->ed.replaceText(input.substr(2,input.size()-1));
-    	else if (input.at(0) == 'j')
-    		this->ed.combine();
-    	else if (input.at(0) == 'w')
-    		this->ed.writeToFile(input.substr(2,input.size()-1));
-
+		char s = input.at(0);
+		switch (s) {
+			case 'q':
+				return;
+				case '0': case '1':case '2': case '3':case '4': case '5':case '6': case '7':case '8': case '9':{
+					this->ed.changeCurrentLine(stoi(input));
+					break;
+				}
+			case '+':{
+				this->ed.xlinesForward(stoi(input.substr(1,input.size()-1)));
+				break;
+			}
+			case '-':{
+				this->ed.xlinesBack(stoi(input.substr(1,input.size()-1)));
+				break;
+			}
+			case '$': {
+				this->ed.lastLine();
+				break;
+			}
+			case 'a':{
+				this->ed.addAfterCurrent();
+				break;
+			}
+			case 'i':{
+				this->ed.addBeforeCurrent();
+				break;
+			}
+			case 'c':{
+				this->ed.changeCurrent();
+				break;
+			}
+			case 'd':{
+				this->ed.deleteCurrent();
+				break;
+			}
+			case '/':{
+				this->ed.search(input.substr(1,input.size()-2));
+				break;
+			}
+			case 's':{
+				cout << "sending : " << input.substr(2,input.size()-3) << endl;
+				this->ed.replaceText(input.substr(2,input.size()-3));
+				break;
+			}
+			case 'j':{
+				this->ed.combine();
+				break;
+			}
+			case 'w': {
+				this->ed.writeToFile(input.substr(2,input.size()-1));
+				break;
+			}
 		}
+
+    }
 
 }
 
